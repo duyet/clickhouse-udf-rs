6297122089e15babcb4cde2b279166639414afd2
Duyet Le <me@duyet.net>
2024-03-01 23:12:17 +0700

    feat: impl README.md generator

---
+++ README.tpl
@@ -0,0 +1,113 @@
+# ClickHouse UDF written in Rust 
+
+Collection of some useful UDFs for ClickHouse written in Rust.
+
+Compile into binary
+
+```bash
+$ cargo build --release
+
+$ ls -lhp target/release | grep -v '/\|\.d'
+{% for project in projects -%}{% for bin in project.bins -%}
+-rwxr-xr-x    1 duet  staff   434K Feb 24 21:26 {{ bin}}
+{% endfor %}{% endfor %}
+```
+
+{% for project in projects -%}
+{{ loop.index }}. [{{ project.name }}](#{{ loop.index }}-{{ project.name | slugify }})
+{% endfor %}
+
+# Usage
+
+{% for project in projects -%}
+
+## {{ loop.index }}. `{{ project.name }}`
+
+
+<details>
+  <summary>
+    Put the <strong>{{ project.name }}</strong> binaries into <code>user_scripts</code> folder (<code>/var/lib/clickhouse/user_scripts/</code> with default path settings).
+  </summary>
+
+  ```bash
+  $ cd /var/lib/clickhouse/user_scripts/
+  $ wget https://github.com/duyet/clickhouse-udf-rs/releases/download/{{ version }}/clickhouse_udf_{{ project.name }}_v{{ version }}_x86_64-unknown-linux-musl.tar.gz
+  $ tar zxvf clickhouse_udf_{{ project.name }}_v{{ version }}_x86_64-unknown-linux-musl.tar.gz
+
+  {% for bin in project.bins -%}
+  {{ bin }}
+  {% endfor %}
+  ```
+</details>
+
+<details>
+  <summary>
+    Creating UDF using XML configuration <code>custom_udf_{{ project.name }}_function.xml</code>
+  </summary>
+
+  define udf config file `{{ project.name }}_udf_function.xml` (`/etc/clickhouse-server/custom_udf_{{ project.name }}_function.xml` with default path settings,
+  file name must be matched `*_function.xml`).
+
+
+  ```xml
+  <functions>
+    <!-- {{ project.name }} -->
+    {% for bin in project.bins -%}
+    <function>
+        <name>{{ bin | to_clickhouse_function }}</name>
+        <type>executable_pool</type>
+        <command>{{ bin }}</command>
+        <format>tabseparated</format>
+        <argument>
+            <type>string</type>
+            <name>value</name>
+        </argument>
+        <return_type>string</return_type>
+    </function>
+    {%- endfor %}
+  </functions>
+  ```
+</details>
+
+<details>
+  <summary>With <code>send_chunk_header=1</code></summary>
+
+  ```xml
+  <functions>
+      <!-- {{ project.name }} -->
+      {% for bin in project.bins -%}
+      <function>
+          <name>{{ bin | to_clickhouse_function }}</name>
+          <type>executable_pool</type>
+
+          <command>{{ bin }}-chunk-header</command>
+          <send_chunk_header>1</send_chunk_header>
+
+          <format>TabSeparated</format>
+          <argument>
+              <type>String</type>
+              <name>value</name>
+          </argument>
+          <return_type>String</return_type>
+      </function>
+      {% endfor -%}
+  </functions>
+  ```
+
+</details>
+
+<details>
+  <summary>ClickHouse example queries</summary>
+
+  ```sql
+  {% for bin in project.bins -%}
+  SELECT {{ bin | to_clickhouse_function }}('value');
+  {% endfor -%}
+  ```
+</details>
+
+{% endfor %}
+
+# License
+
+MIT
---
+++ shared/src/bin/readme-generator.rs
@@ -0,0 +1,114 @@
+use anyhow::{anyhow, Result};
+use serde::Serialize;
+use std::path::Path;
+use std::{collections::HashMap, env};
+use tera::{Context, Tera};
+
+#[derive(Serialize)]
+struct Project {
+    name: String,
+    bins: Vec<String>,
+}
+
+const IGNORED: [&str; 2] = ["shared", "string"];
+
+struct ToClickHouseFunctionName;
+
+impl tera::Filter for ToClickHouseFunctionName {
+    fn filter(
+        &self,
+        value: &tera::Value,
+        _: &HashMap<String, tera::Value>,
+    ) -> tera::Result<tera::Value> {
+        match value {
+            tera::Value::String(bin) => Ok(tera::Value::String(change_case::camel_case(bin))),
+            _ => Err("Expected a string".into()),
+        }
+    }
+}
+
+fn get_bins(member: String) -> Result<Vec<String>> {
+    let child_path = format!("{}/Cargo.toml", member);
+
+    let mut manifest = cargo_toml::Manifest::from_path(Path::new(&child_path))?;
+    manifest.complete_from_path(Path::new(&child_path))?;
+
+    let bins = manifest
+        .bin
+        .into_iter()
+        .map(|bin| bin.name.unwrap_or_default())
+        .collect::<Vec<_>>();
+
+    dbg!(&bins);
+
+    Ok(bins)
+}
+
+/// Get a list of projects from the current workspace
+fn get_projects() -> Result<Vec<Project>> {
+    let mut manifest = cargo_toml::Manifest::from_path(Path::new("Cargo.toml"))?;
+    manifest.complete_from_path(Path::new("Cargo.toml"))?;
+
+    dbg!(&manifest);
+
+    match manifest.workspace {
+        Some(ref workspace) => Ok(workspace
+            .clone()
+            .members
+            .into_iter()
+            .filter(|member| !IGNORED.contains(&member.as_str()))
+            .map(|ref member| {
+                let project = Project {
+                    name: member.to_string(),
+                    bins: get_bins(member.to_string()).unwrap_or_default(),
+                };
+
+                project
+            })
+            .collect::<Vec<_>>()),
+        None => {
+            return Err(anyhow!("No workspace found"));
+        }
+    }
+}
+
+fn get_tera_context() -> Result<Context> {
+    let mut context = Context::new();
+
+    // TODO: Binding the latest version from Cargo.toml
+    context.insert("version", "0.1.5");
+
+    // Binding a list of projects
+    let projects = get_projects()?;
+    context.insert("projects", &projects);
+
+    Ok(context)
+}
+
+/// Usage: cargo run --bin readme-generator -- README.tpl
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let root = args.get(1).expect("Missing template file");
+
+    // Create a new Tera instance and add a template from a string
+    let mut tera =
+        Tera::new(&format!("{}/**/*.tpl", root)).expect("Could not create Tera instance");
+    tera.register_filter("to_clickhouse_function", ToClickHouseFunctionName);
+
+    // Prepare the context with some data
+    let context = get_tera_context()?;
+
+    // List template
+    let names: Vec<_> = tera.get_template_names().collect();
+    match names.contains(&"README.tpl") {
+        true => {
+            let rendered = tera
+                .render("README.tpl", &context)
+                .expect("Could not render README");
+            println!("{}", rendered);
+        }
+        false => println!("No README.tpl found"),
+    }
+
+    Ok(())
+}
