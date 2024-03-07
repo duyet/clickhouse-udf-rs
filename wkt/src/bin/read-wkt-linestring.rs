e715d8d3fb2771ce69758419ed59438b0ef2e7df
Duyet Le <me@duyet.net>
2024-03-07 14:49:57 +0700

    feat: add wkt_parser bench

--- wkt/src/bin/read-wkt-linestring.rs
+++ wkt/src/bin/read-wkt-linestring.rs
@@ -1,48 +1,10 @@
 use anyhow::Result;
-use geo_types::{CoordNum, LineString};
 use shared::io::process_stdin;
-use wkt::TryFromWkt;
 
-/// Converts a linestring to a string in format
-fn to_string<T: CoordNum + std::fmt::Display>(linestring: LineString<T>) -> String {
-    let mut result = "".to_string();
-
-    for point in linestring {
-        result.push_str(&format!("({},{}),", point.x, point.y));
-    }
-
-    // Remove trailing comma and space
-    result.pop();
-
-    format!("[{}]", result)
-}
-
-fn parse_wkt(s: &str) -> Option<String> {
-    match LineString::<f64>::try_from_wkt_str(s) {
-        Ok(linestring) => Some(to_string(linestring)),
-        Err(_err) => Some("".to_string()),
-    }
-}
+use parse_wkt::parse_wkt::parse_wkt;
 
 fn main() -> Result<()> {
     process_stdin(parse_wkt);
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_valid_input() {
-        let input = "LINESTRING(0 0,    1 1,    2 2)";
-        assert_eq!(parse_wkt(input).unwrap(), "[(0,0),(1,1),(2,2)]");
-    }
-
-    #[test]
-    fn test_invalid_input() {
-        let input = "LINESTRING(0 0, 1 1, 2 2";
-        assert_eq!(parse_wkt(input).unwrap(), "");
-    }
 }
